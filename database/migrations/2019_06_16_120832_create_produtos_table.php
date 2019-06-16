8804de2360f337a2bd27bee15a0e8b2c193385cd
guiamado <amado.silva133@gmail.com>
2019-06-16 09:21:16 -0300

    [ADD] Adicionando Migrações

---
+++ database/migrations/2019_06_16_120832_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->float('valor_unitario');
+            $table->integer('quantidade_estoque');
+            $table->boolean('situacao_produto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
