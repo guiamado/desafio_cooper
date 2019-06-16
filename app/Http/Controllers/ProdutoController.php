e7768c7ff5e9e34d35139bf374a9494f47440968
guiamado <amado.silva133@gmail.com>
2019-06-16 12:19:15 -0300

    [REFACTOR] Retirando código não utilizado

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,16 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto as ModeloProduto;
 use App\Services\Produto as ServicoProduto;
-use Illuminate\Http\Request;
 use Psr\Http\Message\ServerRequestInterface;
 
 class ProdutoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return ModeloProduto::latest()->get();
@@ -29,24 +23,11 @@ class ProdutoController extends Controller
         return $response->json($produto->criar($dados));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Produto  $produto
-     * @return \Illuminate\Http\Response
-     */
     public function show(ModeloProduto $produto)
     {
         return $produto;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Produto  $produto
-     * @return \Illuminate\Http\Response
-     */
     public function update(ServerRequestInterface $request, $id)
     {
         $dados = $request->getParsedBody();
@@ -57,12 +38,6 @@ class ProdutoController extends Controller
         return $response->json($produto->alterar($id, $dados));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Produto  $produto
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(ModeloProduto $produto)
     {
         $produto->delete();
