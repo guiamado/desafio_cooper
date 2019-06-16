a6af5b167529bddff0fc368415225f19bab62c20
guiamado <amado.silva133@gmail.com>
2019-06-16 09:20:44 -0300

    [ADD] Adicionando camadas de Model, Controller e Factory

---
+++ database/factories/PedidoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Pedido;
+use Faker\Generator as Faker;
+
+$factory->define(Pedido::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
